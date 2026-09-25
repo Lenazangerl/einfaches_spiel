c4dda59528045091886ccf2674dc04a857b70ea4
Lenazangerl <lena.zangerl123@gmail.com>
2026-09-25 21:04:17 +0200

    Konstruktor, berechneComputerZahl, berechneRunde, logik hinzugefügt

--- src/lzangerl/Model/GewinnModel.java
+++ src/lzangerl/Model/GewinnModel.java
@@ -6,7 +6,12 @@ public class GewinnModel {
     private int computerZahl;
     private int rundenErgebnis;
 
-    public GewinnModel(int sp) {
+    public GewinnModel(int zahl) {
+        this.gesamtPunkte = 30;
+
+        if(zahl > 0 && zahl < 10) {
+            this.spielerZahl = zahl;
+        }
     }
 
     public int getGesamtPunkte() {
@@ -22,9 +27,24 @@ public class GewinnModel {
     }
 
     public void berechneComputerZahl() {
+        int z = (int) (Math.random() * 9) + 1;
+        this.computerZahl = z;
     }
 
     public void berechneRunde(int spielerZahl) {
+        this.spielerZahl = spielerZahl;
+
+        if(spielerZahl == getComputerZahl()) {
+            this.rundenErgebnis = 20;
+        }
+        if((getComputerZahl() - spielerZahl) == -1 || (getComputerZahl() - spielerZahl) == 1) {
+            this.rundenErgebnis = 5;
+        }
+        else {
+            this.rundenErgebnis = -10;
+        }
+
+        this.gesamtPunkte = this.getGesamtPunkte() + this.rundenErgebnis;
     }
 
     public boolean hatGewonnen() {
